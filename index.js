9997ae05930bd3e44ed85546c7304855181c18b1
Your Name <minajmiha02@gmail.com>
2025-10-25 17:50:01 +0600

    .env file update

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ app.use(
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.63qrdth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // const uri = `mongodb://localhost:27017`;
-console.log("Stripe key:", process.env.STRIPE_SECRET_KEY ? "Loaded ✅" : "Missing ❌");
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
