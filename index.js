26040c66f875d4a23f4625f0b016e04399ba735a
Your Name <minajmiha02@gmail.com>
2025-10-25 17:37:00 +0600

    1st commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
 const cors = require("cors");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+app.use(express.json());
 // const cookieParser = require("cookie-parser");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
-require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(
   cors({
@@ -18,11 +19,10 @@ app.use(
   })
 );
 
-// middleware
-app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.63qrdth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // const uri = `mongodb://localhost:27017`;
-console.log(process.env.STRIPE_SECRET_KEY)
+console.log("Stripe key:", process.env.STRIPE_SECRET_KEY ? "Loaded ✅" : "Missing ❌");
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
