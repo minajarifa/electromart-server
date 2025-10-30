bf4b0fecc410877dacff406c542822f32fe36468
Your Name <minajmiha02@gmail.com>
2025-10-30 21:57:18 +0600

    post

--- index.js
+++ index.js
@@ -182,17 +182,27 @@ async function run() {
         });
       }
     );
+    app.get("/payment/:email",verifyToken, async (req, res) => {
+      const query = {email:req.params.email}
+      if(req.params.email !== req.decoded.email){
+        return res.status(403).send({message:"forbidden access"})
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    app.post("/payment", async(req, res) => {
+    app.post("/payment", async (req, res) => {
       const payment = req.body;
       const paymentResult = await paymentCollection.insertOne(payment);
       // carefully delete each item from the cart
-      console.log('payment info',payment);
-      const query =  {_id :  {
-        $in:payment.cartIds.map(id=>new ObjectId(id))
-      }}
-      const deleteResult = await productCollection.deleteMany(query)
-      res.send({paymentResult, deleteResult});
+      console.log("payment info", payment);
+      const query = {
+        _id: {
+          $in: payment.cartIds.map((id) => new ObjectId(id)),
+        },
+      };
+      const deleteResult = await productCollection.deleteMany(query);
+      res.send({ paymentResult, deleteResult });
     });
 
     // payment related api end__________________________________
